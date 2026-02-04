90e635cc8b900378944864a8ef0c9714fd5e5021
HardlinePizza21 <smadrido@eafit.edu.co>
2026-02-04 11:29:44 -0500

    About page added

--- movie/views.py
+++ movie/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('<h1>Welcome  to Home page</h1>')
     return render(request, 'home.html', {'name':'Samuel Madrid Ossa'})
+
+def about(request):
+    return render(request, 'about.html')
+
